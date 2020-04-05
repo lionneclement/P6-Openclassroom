f1ac1936e027faf44049b1d9155ead47b79d3461
clement <lionneclement@gmail.com>
2020-04-05 17:00:03 +0200

    Create a file Token

---
+++ src/Tools/Token.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tools;
+
+class Token
+{
+  public function generator($numberCharacter)
+  {
+    $character = str_split('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789');
+    shuffle($character);
+    $rand = '';
+    foreach (array_rand($character, $numberCharacter) as $k) { 
+        $rand .= $character[$k];
+    }
+    return $rand;
+  }
+}
