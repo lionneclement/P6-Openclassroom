a06fcd12770b70a756ea9d9c39bb7b0ac0b4a46c
clement <lionneclement@gmail.com>
2020-04-05 17:02:44 +0200

    Change template in mail

--- src/Mail/Mail.php
+++ src/Mail/Mail.php
@@ -2,25 +2,30 @@
 
 namespace App\Mail;
 
+use Twig\Environment;
+
 class Mail
 {
     private $mailer;
 
-    public function __construct(\Swift_Mailer $mailer)
+    public function __construct(\Swift_Mailer $mailer, Environment $twig)
     {
         $this->mailer = $mailer;
+        $this->twig = $twig;
     }
-    public function sendEmail()
+    public function forgotPassword(string $token, object $user)
     {
-        $message = (new \Swift_Message('Hello Email'))
-            ->setFrom('send@example.com')
-            ->setTo('recipient@example.com')
+        $message = (new \Swift_Message('Lien pour réinitialiser son mot de passe'))
+            ->setFrom('admin@snowboard.com')
+            ->setTo($user->getEmail())
             ->setBody(
-               'Hey',
+                $this->twig->render(
+                    'mail/forgotPassword.html.twig',
+                    ['name' => $user->getName(), 'token' => $token]
+                ),
                 'text/html'
             )
         ;
-    
         $this->mailer->send($message);
     }
 }
