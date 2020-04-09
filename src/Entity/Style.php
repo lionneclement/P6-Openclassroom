8567c16ca6f122ffd6d7b52e3b9abddef5175541
clement <lionneclement@gmail.com>
2020-04-09 10:53:11 +0200

    add unique value on style name

--- src/Entity/Style.php
+++ src/Entity/Style.php
@@ -2,12 +2,14 @@
 
 namespace App\Entity;
 
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\StyleRepository")
+ * @UniqueEntity("name")
  */
 class Style
 {
@@ -19,7 +21,7 @@ class Style
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     private $name;
 
