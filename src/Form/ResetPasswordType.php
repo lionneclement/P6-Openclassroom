d21dff6c422367bb34d166727da6d761557ea907
clement <lionneclement@gmail.com>
2020-04-11 11:12:46 +0200

    add constrainst in ResetPasswordType

--- src/Form/ResetPasswordType.php
+++ src/Form/ResetPasswordType.php
@@ -7,6 +7,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ResetPasswordType extends AbstractType
 {
@@ -15,7 +17,21 @@ class ResetPasswordType extends AbstractType
         $builder
             ->add(
                 'password', PasswordType::class, [
-                'mapped'=> false
+                'mapped'=> false,
+                'constraints' => [
+                    new NotBlank(
+                        [
+                        'message' => 'Veuillez entrer un mot de passe',
+                        ]
+                    ),
+                    new Length(
+                        [
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit contenir au moins 6 caractères',
+                        'max' => 4096,
+                        ]
+                    ),
+                ],
                 ]
             );
     }
