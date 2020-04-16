cfd22e7dc5b3790b7cee6844249234884dff8b7c
clement <lionneclement@gmail.com>
2020-04-16 17:46:26 +0200

    from profileController and securityController to userController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,7 +1,7 @@
 <?php
 
 /** 
- * The file is for security
+ * The file is for user
  * 
  * PHP version 7.3.5
  * 
@@ -25,13 +25,16 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 use App\Form\ForgotPasswordType;
+use App\Form\ProfileType;
 use App\Form\ResetPasswordType;
 use App\Mail\Mail;
+use App\Tools\File;
 use App\Tools\Token;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /** 
- * The class is for security
+ * The class is for user
  * 
  * @category Controller
  * @package  Controller
@@ -39,7 +42,7 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
  * @license  https://opensource.org/licenses/MIT MIT License
  * @link     http://localhost:8000
  */
-class SecurityController extends AbstractController
+class UserController extends AbstractController
 {
     /**
      * Login
@@ -197,5 +200,46 @@ class SecurityController extends AbstractController
             );
         }
         return $this->redirectToRoute('home_page');
+    }
+    /**
+     * Render profile
+     * 
+     * @param object $user 
+     * @param object $request 
+     * @param object $file 
+     * 
+     * @Route("/auth/profile", name="profile")
+     * 
+     * @return response
+     */
+    public function userProfile(UserInterface $user, Request $request, File $file): Response
+    {
+        $form = $this->createForm(ProfileType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->addFlash('success', 'Votre profile à été modifié');
+            $profile = $form->getData();
+
+            $image = $form['imageName']->getData();
+            if ($image) {
+                if ($user->getImageName() != 'default-user.png') {
+                    $file->removeImage($user->getImageName());
+                }
+                $imageFileName = $file->uploadImage($image);
+                $profile->setImageName($imageFileName);
+            }
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($profile);
+            $entityManager->flush();
+
+            return $this->redirect($request->getUri());
+        }
+        return $this->render(
+            'profile/index.html.twig',
+            [
+                'form' => $form->createView(),
+                'user' => $user,
+            ]
+        );
     }
 }
