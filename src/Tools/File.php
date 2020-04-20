6a3a0c893cbdcc966454cf0affe134145b8a4504
clement <lionneclement@gmail.com>
2020-04-20 16:22:18 +0200

    refactoring tricksController

--- src/Tools/File.php
+++ src/Tools/File.php
@@ -12,6 +12,8 @@
  */
 namespace App\Tools;
 
+use App\Entity\Photo;
+use App\Entity\Tricks;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
@@ -44,7 +46,7 @@ class File
      * 
      * @return string 
      */
-    public function uploadImage(UploadedFile $file)
+    public function uploadImage(UploadedFile $file): string
     {
         $fileName = 'image'.'-'.uniqid().'.'.$file->guessExtension();
 
@@ -60,20 +62,82 @@ class File
      * 
      * @return string 
      */
-    public function getPhotoDirectory()
+    public function getPhotoDirectory(): string
     {
         return $this->_photoDirectory;
     }
     /**
-     * Upload Image
+     * Remove Image
      * 
      * @param string $file 
      * 
      * @return void 
      */
-    public function removeImage(string $file)
+    public function removeImage(string $file): void
     {
         $filesystem = new Filesystem();
         $filesystem->remove([$this->getPhotoDirectory().'/'.$file]);
+    }
+    /**
+     * Add multiple images
+     * 
+     * @param object $images  
+     * 
+     * @return void
+     */
+    public function uploadMultipleImage(Photo $images): void
+    {
+        foreach ($images as $image) {
+            $imageFileName = $this->uploadImage($image->getFile());
+            $image->setName($imageFileName);
+        }
+    }
+    /**
+     * Update multiple images
+     * 
+     * @param object $images  
+     * 
+     * @return void
+     */
+    public function updateMultipleImage(object $images): void
+    {
+        foreach ($images as $image) {
+            if ($image->getFile()) {
+                if ($image->getId()) {
+                    $this->removeImage($image->getName());
+                }
+                $imageFileName = $this->uploadImage($image->getFile());
+                $image->setName($imageFileName);
+            }
+        }
+    }
+    /**
+     * Remove multiple old image
+     * 
+     * @param object $images 
+     * @param object $trick 
+     * 
+     * @return void
+     */
+    public function removeMultipleOldImage(object $images,Tricks $trick): void
+    {
+        foreach ($images as $image) {
+            if (!$trick->getPhotos()->contains($image)) {
+                $this->removeImage($image->getName());
+            }
+        }
+    }
+    /**
+     * Remove multiple image
+     * 
+     * @param object $images 
+     * 
+     * @return void
+     */
+    public function removeMultipleImage(array $images): void
+    {
+        foreach ($images as $image) {
+            $this->removeImage($image->getName());
+        }
     }
 }
